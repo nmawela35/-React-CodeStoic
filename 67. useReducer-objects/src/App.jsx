b58e5bd2ad480f8f4f4a8230ffabb4dbbb9fbf20
Christopher Mawela <christopher.mawela@gmail.com>
2024-09-03 20:13:53 +0200

    Tutorial 67 complete, useReducer needs practice

--- 67. useReducer-objects/src/App.jsx
+++ 67. useReducer-objects/src/App.jsx
@@ -1,24 +1,28 @@
 import { useReducer} from 'react';
 
 function App() {
-  const [state,dispatch] = useReducer(reducer,{count:0})
-
-  // const initValue=0
+  const [state,dispatch] = useReducer(reducer,{count:0,incrementBy:5})
 
 function reducer(state,action){
   if (action.type === 'increment'){
-    return {...state,count:state.count+ action.payload}
+    return {...state,count:state.count+ state.incrementBy}
   } 
   if (action.type == 'decrement'){
-    return {...state,count:state.count- action.payload}
+    return {...state,count:state.count- state.incrementBy}
+  }
+  if (action.type=="setIncrement"){
+    return {...state,incrementBy:action.payload}
+  }
 
-  }}
+  }
 
 
   return (
     <div className="App">
 
+
       <h1>Use Reducer {" "}{state.count}</h1>
+      <input type="text" value={state.incrementBy} onChange={(e)=>dispatch({type:"setIncrement",payload:Number(e.target.value)})}/>
       <button onClick={() =>dispatch({type:"increment", payload:5})}>Increment</button>
       <button onClick={() =>dispatch({type:"decrement",payload:3})}>Decrement</button>
 
